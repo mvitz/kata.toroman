0e781aacb386536a4e33d478ca10cb24b64358b9
Michael Vitz <vitz.michael@googlemail.com>
2014-07-22 23:05:49 +0200

    Add steps to implementation.

--- src/main/java/de/mvitz/kata/toroman/ToRoman.java
+++ src/main/java/de/mvitz/kata/toroman/ToRoman.java
@@ -1,8 +1,24 @@
 package de.mvitz.kata.toroman;
 
+import java.util.Collection;
+
 public final class ToRoman {
 
     public String convert(int arabicNumber) {
+        final Collection<Integer> factors = findFactors(arabicNumber);
+        final Collection<String> romanDigits = translate(factors);
+        return compile(romanDigits);
+    }
+
+    private Collection<Integer> findFactors(int arabicNumber) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    private Collection<String> translate(Collection<Integer> factors) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    private String compile(Collection<String> romanDigits) {
         throw new UnsupportedOperationException("Not implemented!");
     }
 
