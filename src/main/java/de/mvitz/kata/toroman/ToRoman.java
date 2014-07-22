16b664b0661b80569de6fcaa612938e221d7e0b1
Michael Vitz <vitz.michael@googlemail.com>
2014-07-23 00:15:44 +0200

    Refactoring.

--- src/main/java/de/mvitz/kata/toroman/ToRoman.java
+++ src/main/java/de/mvitz/kata/toroman/ToRoman.java
@@ -5,6 +5,23 @@ import java.util.stream.Collectors;
 
 public final class ToRoman {
 
+    private final Map<Integer, String> map = new HashMap<>();
+
+    public ToRoman() {
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
+    }
+
     public String convert(int arabicNumber) {
         final Collection<Integer> factors = findFactors(arabicNumber);
         final Collection<String> romanDigits = translate(factors);
@@ -12,7 +29,8 @@ public final class ToRoman {
     }
 
     Collection<Integer> findFactors(int arabicNumber) {
-        final Collection<Integer> possibleFactors = Arrays.asList(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1);
+        final Collection<Integer> possibleFactors = map.keySet().stream().sorted(Comparator.<Integer>reverseOrder()).collect(Collectors.toList());
+
         final Collection<Integer> factors = new ArrayList<>();
         while (arabicNumber > 0) {
             final int factor = findFactor(arabicNumber, possibleFactors);
@@ -27,21 +45,7 @@ public final class ToRoman {
     }
 
     Collection<String> translate(Collection<Integer> factors) {
-        final Map<Integer, String> arabicToRoman = new HashMap<>();
-        arabicToRoman.put(1, "I");
-        arabicToRoman.put(4, "IV");
-        arabicToRoman.put(5, "V");
-        arabicToRoman.put(9, "IX");
-        arabicToRoman.put(10, "X");
-        arabicToRoman.put(40, "XL");
-        arabicToRoman.put(50, "L");
-        arabicToRoman.put(90, "XC");
-        arabicToRoman.put(400, "CD");
-        arabicToRoman.put(500, "D");
-        arabicToRoman.put(900, "CM");
-        arabicToRoman.put(1000, "M");
-
-        return factors.stream().map(arabicToRoman::get).collect(Collectors.toList());
+        return factors.stream().map(map::get).collect(Collectors.toList());
     }
 
     String compile(Collection<String> romanDigits) {
